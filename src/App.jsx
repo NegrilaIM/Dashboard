fc045138ff81483b48daf836c2c9ff1899b012ac
NegrilaIM <negrilaim@gmail.com>
2024-08-28 19:36:10 +0300

    added Products/Users/Sales pages + components.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom"
 import OverviewPage from "./pages/OverviewPage"
 import ProductsPage from "./pages/ProductsPage"
 import Sidebar from "./components/Sidebar"
+import UsersPage from './pages/UsersPage'
+import SalesPage from "./pages/SalesPage"
+import OrdersPage from "./pages/OrdersPage"
 
 function App() {
 
@@ -9,7 +12,6 @@ function App() {
     <>
       <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
         <div className="fixed inset-0 z-0">
-          {/* Gradient de fundal mai vibrant */}
           <div className="absolute inset-0 bg-gradient-to-br from-cyan-800 via-blue-700 to-indigo-700 opacity-60" />
 
 
@@ -20,6 +22,9 @@ function App() {
         <Routes>
           <Route path="/" element={<OverviewPage />} />
           <Route path="/products" element={<ProductsPage />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path='/sales' element={<SalesPage />} />
+          <Route path='/orders' element={<OrdersPage />} />
         </Routes>
       </div>
     </>
